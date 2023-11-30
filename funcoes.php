bb4401ce3ab0222bd1d67c9e7919c9f81fa1bb8f
wellysonlee <martinsgwellyson@gmail.com>
2023-11-30 16:46:49 -0300

    arquivo separado

---
+++ funcoes.php
@@ -0,0 +1,32 @@
+<?php
+
+// função de deposito
+function depositar(array $conta, float $valorADepositar): array
+{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depositos precisam ser positivos");
+    }
+    return $conta;
+} 
+
+
+//função de saque
+
+function sacar(array $conta, float $valorASacar): array
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não tem saldo suficiente");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
+// função de exibir mensagem padrão
+function exibeMensagem(string $mensagem)
+{
+    echo $mensagem . PHP_EOL;
+}
