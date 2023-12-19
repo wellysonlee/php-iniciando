70a3788361cebc7f5e7f156b393cd97df7ee377a
wellysonlee <martinsgwellyson@gmail.com>
2023-12-19 15:43:04 -0300

    explode

--- usuario.php
+++ usuario.php
@@ -1,5 +1,6 @@
 <?php
 
+$nome = 'Wellyson Lee';
 $email = 'wellyson@gmail.com';
 $senha = '1éllll6';
 
@@ -15,3 +16,8 @@ $usuario= substr($email, 0, $posicaoArroba);
 
 echo mb_strtoupper($usuario).PHP_EOL;
 echo substr($email, $posicaoArroba + 1).PHP_EOL;
+
+list($nome, $sobrenome) = explode(' ', $nome);
+
+echo 'Nome: '.$nome.PHP_EOL;
+ECHO 'Sobrenome: '.$sobrenome.PHP_EOL; 
